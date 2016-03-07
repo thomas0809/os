4b5a133e21d5f11a65bc318a0212503259c55d90
钱雨杰 <thomasq0809@gmail.com>
2016-03-07 19:03:33 +0800

    comment

--- lec5/malloc.c
+++ lec5/malloc.c
@@ -4,7 +4,7 @@
 
 #define SIZE 1000000
 
-struct node{
+struct node{              // 链表的数据结构，用于维护内存块的信息
 	int size;
 	void* first;
 	char dirty;
@@ -15,9 +15,9 @@ struct node *start;
 void *p;
 
 
-void *mymalloc(int size)
+void *mymalloc(int size) // 申请内存
 {
-	if(p == NULL) {
+	if(p == NULL) {      // 初始化申请一块内存，并初始化链表
 		p = sbrk(SIZE);
 		start = sbrk(sizeof(struct node));
 		start->size = SIZE;
@@ -27,7 +27,7 @@ void *mymalloc(int size)
 	}
 
 	struct node *temp = start;
-	while(temp != NULL) {
+	while(temp != NULL) {    // 遍历链表，找到第一块符合要求的内存块
 		if(temp->size > size && temp->dirty == 0) {
 			struct node *temp2 = sbrk(sizeof(struct node));
 			temp2->size = temp->size - size;
@@ -48,20 +48,20 @@ void *mymalloc(int size)
 	return NULL;
 }
 
-int myfree(void *pt)
+int myfree(void *pt) // 释放内存
 {
 	struct node *pred = NULL, *succ = NULL;
 	struct node *temp = start;
 	while(temp != NULL) {
-		if (temp->first == pt && temp->dirty == 1) {
+		if (temp->first == pt && temp->dirty == 1) {  // 在链表中找到需要释放的内存块
 			temp->dirty = 0;
-			if (temp->next != NULL && temp->next->dirty == 0) {
+			if (temp->next != NULL && temp->next->dirty == 0) { // 与后面的内存块合并
 				temp->size += temp->next->size;
 				succ = temp->next;
 				temp->next = temp->next->next;
 				//free(succ);
 			}
-			if (pred != NULL && pred->dirty == 0) {
+			if (pred != NULL && pred->dirty == 0) {  // 与前面的内存快合并
 				pred->size += temp->size;
 				pred->next = temp->next;
 				//free(temp);
